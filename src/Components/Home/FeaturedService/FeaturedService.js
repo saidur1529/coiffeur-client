fac653a3f22db4dd69c8d63f22c8315cbc63c3d9
saidur1529 <saidur.rahman04@northsouth.edu>
2021-04-23 12:32:10 +0600

    route fixed

--- src/Components/Home/FeaturedService/FeaturedService.js
+++ src/Components/Home/FeaturedService/FeaturedService.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import featuredImg from '../../../Images/featured_services.jpg';
 const FeaturedService = () => {
     return (
         <section className="features-service pb-0 pb-md-5 mt-5">
             <div className="container mb-5">
                 <div className="row mb-5">
                     <div className="col-md-5 mb-4 m-md-0">
-                        <img className="img-fluid" src="" alt=""/>
+                        <img className="img-fluid w-75" src={featuredImg} alt=""/>
                     </div>
                     <div className="col-md-7 align-self-center">
                         <h1>Exceptional Salon Service, on your Term</h1>
